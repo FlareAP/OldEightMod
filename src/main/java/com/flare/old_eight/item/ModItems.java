b1799f341273272d90196f03fa6bff13dc6a501e
FlareAP <1625053741@qq.com>
2023-05-23 23:45:02 +0800

    register things in their own classes

--- src/main/java/com/flare/old_eight/item/ModItems.java
+++ src/main/java/com/flare/old_eight/item/ModItems.java
@@ -1,7 +1,22 @@
 package com.flare.old_eight.item;
 
+import com.flare.old_eight.Old_eight;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import static com.flare.old_eight.Old_eight.MOD_ID;
 
 public class ModItems {
-    public static final Item LAOBA = new LaobaItem(new Item.Settings().food(ModFoodComponents.LAOBA));
+    public static final Item LAOBA = register("laoba", new LaobaItem(new FabricItemSettings().food(ModFoodComponents.LAOBA)));
+
+    private static Item register(String id, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(MOD_ID, id), item);
+    }
+
+    public static void registerItem(){
+        Old_eight.LOGGER.info("Registering mod items for " + MOD_ID);
+    }
 }
--- src/main/java/com/flare/old_eight/sound/ModSoundEvents.java
+++ src/main/java/com/flare/old_eight/sound/ModSoundEvents.java
@@ -1,13 +1,23 @@
 package com.flare.old_eight.sound;
 
+import com.flare.old_eight.Old_eight;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
 import static com.flare.old_eight.Old_eight.MOD_ID;
 
 public class ModSoundEvents {
-    public static final Identifier OLIGEI = new Identifier(MOD_ID, "oligei");
-    public static final Identifier YUE = new Identifier(MOD_ID, "yue");
-    public static final SoundEvent OLD_EIGHT_OLIGEI = SoundEvent.of(OLIGEI);
-    public static final SoundEvent OLD_EIGHT_YUE = SoundEvent.of(YUE);
+    public static final SoundEvent OLD_EIGHT_OLIGEI = register("oligei");
+    public static final SoundEvent OLD_EIGHT_YUE = register("yue");
+
+    private static SoundEvent register(String id) {
+        Identifier identifier = new Identifier(MOD_ID, id);
+        return Registry.register(Registries.SOUND_EVENT, identifier, SoundEvent.of(identifier));
+    }
+
+    public static void registerSoundEvent() {
+        Old_eight.LOGGER.info("Registering mod sound events for " + MOD_ID);
+    }
 }
