2b421cd662db57801dde2a59e8f9efd43c29f1ad
FlareAP <1625053741@qq.com>
2023-05-28 17:03:53 +0800

    rename main class

--- src/main/java/com/flare/old_eight/OldEight.java
+++ src/main/java/com/flare/old_eight/OldEight.java
@@ -1,20 +1,22 @@
 package com.flare.old_eight;
 
 import com.flare.old_eight.block.ModBlocks;
+import com.flare.old_eight.item.ModItemGroups;
 import com.flare.old_eight.item.ModItems;
 import com.flare.old_eight.sound.ModSoundEvents;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Old_eight implements ModInitializer {
+public class OldEight implements ModInitializer {
     public static final String MOD_ID = "old_eight";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        ModItems.registerItem();
+        ModItemGroups.registerItemGroup();
         ModSoundEvents.registerSoundEvent();
+        ModItems.registerItem();
         ModBlocks.registerBlock();
     }
 }
